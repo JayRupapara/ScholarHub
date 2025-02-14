265db7563fa5f6a150e719b4d01bb08f881c5853
Dhaval Agnani <dhavalagnani2684@gmail.com>
2025-02-15 04:24:38 +0530

    `Update user.model.js and user.controller.js files with minor schema and code changes`

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,75 +1,72 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
-  fullName: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  mobileNo: {
-    type: Number,
-    required: true,
-  },
-  documents: [{
-    documentType: {
+const userSchema = new mongoose.Schema(
+  {
+    fullName: {
       type: String,
+      required: true,
     },
-    documentId: {
+    email: {
       type: String,
+      required: true,
     },
-    documentURL: {
+    password: {
       type: String,
+      required: true,
     },
-    verificationStatus: {
+    mobileNo: {
+      type: Number,
+      required: true,
+    },
+    documents: [
+      {
+        documentType: {
+          type: String,
+        },
+        documentId: {
+          type: String,
+        },
+        documentURL: {
+          type: String,
+        },
+        verificationStatus: {
+          type: Boolean,
+          default: false,
+        },
+      },
+    ],
+    highestQualification: {
+      type: String,
+      enum: ["10th", "12th", "UG", "PG", "PHD"],
+    },
+    gender: {
+      type: String,
+      enum: ["Male", "Female"],
+    },
+    disability: {
       type: Boolean,
       default: false,
-    }
-  }],
-  highestQualification: {
-    type: String,
-    enum: ["10th", "12th", "UG", "PG", "PHD"]
-  },
-  gender:{
-    type: String,
-    enum: ["Male","Female"]
-  },
-  disability: {
-    type: Boolean,
-    default: false,
-  },
-  caste: {
-    type: String,
-    enum: ['General', 'ST', 'SC', 'OBC']
-  },
-  familyIncome: {
-    type: Number,
-  },
-  DOB: {
-    type: Date
-  },
-  bankDetails: {
-    accountNumber: {
+    },
+    caste: {
+      type: String,
+      enum: ["General", "ST", "SC", "OBC"],
+    },
+    familyIncome: {
       type: Number,
     },
-    IFSC: {
-      type: String,
+    DOB: {
+      type: Date,
     },
-    bankName: {
-      type: String,
-    },
-    branchName: {
-      type: String,
-    },
+    savedScholarships: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Scholarships",
+      },
+    ],
   },
-}, {
-  timestamps: true,
-});
+  {
+    timestamps: true,
+  }
+);
 
 export default mongoose.model("User", userSchema);
